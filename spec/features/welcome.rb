92d738f1533456310dac1201aa79a74a83f7450b
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-23 06:24:55 -0600

    Testing for presence of links on welcome page poassing. Moving to redirect expectations.

--- spec/features/welcome.rb
+++ spec/features/welcome.rb
@@ -7,10 +7,10 @@ RSpec.describe 'Features', type: :feature do
             expect(page).to have_content('Welcome!')
         end
 
-        xit 'has links to dog and cats index pages' do
+        it 'has links to dog and cats index pages' do
             visit '/'
-            endexpect(page).to have_link('Dogs')
-            endexpect(page).to have_link('Cats')
+            expect(page).to have_link('Dogs')
+            expect(page).to have_link('Cats')
         end
     end
 end
