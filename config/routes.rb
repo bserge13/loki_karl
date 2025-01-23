c76755c2d1a4853a0555e5612d3cc6e61e564b51
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-23 06:58:43 -0600

    Dogs index page routing working. Moving to Cats index.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # landing page
   get '/', to: 'welcome#index'
-  # get '/dogs', to: 'dogs#index'
+
+  # dogs
+  get '/dogs', to: 'dogs#index'
 end
--- spec/features/welcome.rb
+++ spec/features/welcome.rb
@@ -2,21 +2,29 @@ require 'rails_helper'
 
 RSpec.describe 'Features', type: :feature do
     describe 'Welcome page' do 
-        it 'has a landing page' do 
+        before :each do
             visit '/'
+        end
+
+        it 'has a landing page' do 
             expect(page).to have_content('Welcome!')
         end
 
         it 'has links to dog and cats index pages' do
-            visit '/'
             expect(page).to have_link('Dogs')
             expect(page).to have_link('Cats')
         end
 
-        it 'redirects to a dog or cat index page' do 
-            visit '/'
+        it 'redirects to a dog index page' do 
             click_link('Dogs')
             expect(current_path).to eq('/dogs')
+            expect(page).to have_content('Dogs Page!')
+        end
+
+        it 'redirects to a cat index page' do 
+            click_link('Cats')
+            expect(current_path).to eq('/cats')
+            expect(page).to have_content('Cats Page!')
         end
     end
 end
