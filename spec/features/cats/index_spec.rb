87bcf2b4c8b8d27856fefa696d0f4668991fb51e
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-24 06:10:54 -0600

    Completion of test writing, moving to TDD.

--- spec/features/cats/index_spec.rb
+++ spec/features/cats/index_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'Cats Features'm type: :feature do 
+RSpec.describe 'Cats Features', type: :feature do 
     describe 'Cats page' do 
         before :each do 
             @karl = Cat.create!(name: 'Karl', age: 1, color: 'Grey')
             @cowboy = Cat.create!(name: 'Cowboy', age: 1, color: 'Grey')
             @bexley = Cat.create!(name: 'Bexley', age: 1, color: 'Brown')
         end
+
         it 'has an index page with cats' do
             visit '/cats'
             within "#cats-#{@karl.id}" do
-                expect(page).to have_content(@karl.name)
-                expect(page).to have_content(@karl.age)
-                expect(page).to have_content(@karl.color)
+                expect(page).to have_content("Name: #{@karl.name}")
+                expect(page).to have_content("Age: #{@karl.age}")
+                expect(page).to have_content("Coat: #{@karl.color}")
             end
         end
     end
