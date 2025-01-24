47ae18f4f9c2e9fb4ceddc567de02d73f295d374
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-24 06:19:42 -0600

    Refactor of testing format.

--- spec/features/cats/index_spec.rb
+++ spec/features/cats/index_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Cats Features', type: :feature do 
     describe 'Cats page' do 
-        before :each do 
+        it 'has an index page of cats and their attrs' do
             @karl = Cat.create!(name: 'Karl', age: 1, color: 'Grey')
             @cowboy = Cat.create!(name: 'Cowboy', age: 1, color: 'Grey')
             @bexley = Cat.create!(name: 'Bexley', age: 1, color: 'Brown')
-        end
 
-        it 'has an index page with cats' do
             visit '/cats'
-            
+
             within "#cats-#{@karl.id}" do
                 expect(page).to have_content("Name: #{@karl.name}")
                 expect(page).to have_content("Age: #{@karl.age}")
