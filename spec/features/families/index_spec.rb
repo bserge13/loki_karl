c378f6951eddeff5257e000ab71d26fefe74407e
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-25 08:46:35 -0600

    Instantiation and test writing complete, moving to the two Deez.

--- spec/features/families/index_spec.rb
+++ spec/features/families/index_spec.rb
@@ -5,11 +5,24 @@ RSpec.describe 'Families Features', type: :feature do
         before :each do 
             @karl = Cat.create!(name: 'Karl', age: 1, color: 'Grey')
             @loki = Dog.create!(name: 'Loki', age: 5, color: 'Blue')
-        end
-        it 'has an index of a cats associated dogs' do
-            visit "/cats/#{@karl.id}"
+            @nova = Dog.create!(name: 'Nova', age: 8, color: 'Yellow')
+            @heidi = Dog.create!(name: 'Heidi', age: 10, color: 'White')
+
+            Family.create!(dog_id: @loki.id, cat_id: @karl.id)
+            Family.create!(dog_id: @nova.id, cat_id: @karl.id)
+            Family.create!(dog_id: @heidi.id, cat_id: @karl.id)
         end
         
+        it 'has an index of a cats associated dogs' do
+            visit "/cats/#{@karl.id}"
+            click_link("#{@karl.name}'s Dogs")
+
+            expect(page).to have_content("#{@karl}'s Family")
+            expect(page).to have_content(@loki.name)
+            expect(page).to have_content(@nova.name)
+            expect(page).to have_content(@heidi.name)
+        end
+
         xit 'has an index of a dogs associated cats' do
             visit "/dogs/#{@loki.id}"
 
