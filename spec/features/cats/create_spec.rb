8090b9b4e5a592f91dd3d97622db5396d3398ec3
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-29 06:34:12 -0600

    Test writing complete. Moving to the errors.

--- spec/features/cats/create_spec.rb
+++ spec/features/cats/create_spec.rb
@@ -6,10 +6,26 @@ RSpec.describe 'Cats Features', type: :feature do
             @karl = Cat.create!(name: 'Karl', age: 1, color: 'Brown')
             @cowboy = Cat.create!(name: 'Cowboy', age: 1, color: 'Grey')
             @bexley = Cat.create!(name: 'Bexley', age: 1, color: 'Brown')
-        end
-        it 'has a form page to create new cats' do
+
             visit cats_path
-            save_and_open_page
+        end
+        it 'has form field(s) to create new cats' do
+            expect(page).to have_field("Name")
+            expect(page).to have_field("Age")
+            expect(page).to have_field("Color")
+            expect(page).to have_button("Create")
+        end
+
+        it 'creates new cats' do
+            expect(page).to_not have_content("Greyling")
+
+            fill_in "Name", with: "Greyling"
+            fill_in "Age", with: 8
+            fill_in "Color", with: "Grey"
+            click_button "Create"
+
+            expect(current_path).to eq(cats_path)
+            expect(page).to have_content("Greyling")
         end
     end
 end
