f12447574237a779a8979e9c92d8536c7ce8b8c7
Blake Sergesketter <blakesergesketter@gmail.com>
2025-01-31 06:44:10 -0600

    Model method average_age for cats class intialized and tested. All passing Skrrt.

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,4 +3,9 @@ class Cat < ApplicationRecord
     validates_numericality_of :age
     has_many :families
     has_many :dogs, through: :families
+
+    def self.average_age
+        # require 'pry'; binding.pry
+        average(:age).round(2)
+    end
 end
--- spec/features/cats/index_spec.rb
+++ spec/features/cats/index_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Cats Features', type: :feature do 
     describe 'Cats index page' do 
-        it 'has an index page of cats and their attrs' do
-            @karl = Cat.create!(name: 'Karl', age: 1, color: 'Brown')
+        before :each do 
+            @karl = Cat.create!(name: 'Karl', age: 2, color: 'Brown')
             @cowboy = Cat.create!(name: 'Cowboy', age: 1, color: 'Grey')
-            @bexley = Cat.create!(name: 'Bexley', age: 1, color: 'Brown')
-
+            @bexley = Cat.create!(name: 'Bexley', age: 5, color: 'Brown')
+            
             visit cats_path
-
+        end
+        it 'has an index page of cats and their attrs' do
             within "#cats-#{@karl.id}" do
                 expect(page).to have_content("Name: #{@karl.name}")
                 expect(page).to have_content("Age: #{@karl.age}")
@@ -18,6 +19,12 @@ RSpec.describe 'Cats Features', type: :feature do
             expect(page).to have_link(@cowboy.name)
             click_link(@cowboy.name)
             expect(current_path).to eq("/cats/#{@cowboy.id}")
+        end
+
+        it 'lists the average age of all the cats listed' do
+            expect(page).to have_content("Average age")
+            expect(page).to have_content(Cat.average_age)
+            expect(Cat.average_age).to eq(2.67)
         end
     end
 end
