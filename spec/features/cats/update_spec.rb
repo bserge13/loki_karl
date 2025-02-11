4060b68bda46df4aa2649f50099d3c08734c92b2
Blake Sergesketter <blakesergesketter@gmail.com>
2025-02-11 06:45:52 -0600

    Completion of update page with fields pre-filled with existing values. Testing passes. Yeet.

--- spec/features/cats/update_spec.rb
+++ spec/features/cats/update_spec.rb
@@ -15,5 +15,15 @@ RSpec.describe 'Cats Feautres', type: :feature do
                 expect(current_path).to eq(edit_cat_path(@logan.id))
             end
         end
+
+        it 'has an update page with form fields' do 
+            visit edit_cat_path(@logan.id)
+
+            expect(page).to have_content("Update #{@logan.name}")
+            expect(page).to have_field('Name')
+            expect(page).to have_field('Age')
+            expect(page).to have_field('Color')
+            expect(page).to have_button('Update')
+        end
     end
 end
