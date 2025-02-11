c0a1d8cc47bb2cb1c51417dd3d73e65d530ad4d5
Blake Sergesketter <blakesergesketter@gmail.com>
2025-02-11 06:35:19 -0600

    Tweking to button on views page. Added another Cat instantiation for testing purposes. All good, SkeeYee.

--- spec/features/cats/update_spec.rb
+++ spec/features/cats/update_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Cats Feautres', type: :feature do
     describe 'Cats update action' do 
-        it 'can update an existing cat' do 
+        before :each do 
             @logan = Cat.create!(name: 'Logan', age: 2, color: 'Black')
-
+            @karl = Cat.create!(name: 'Karl', age: 2, color: 'Brown')
+        end
+        it 'can update an existing cat' do 
             visit cats_path
 
             within "#cats-#{@logan.id}" do 
-                expect(page).to have_button('Update')
-                click_button('Update')
+                expect(page).to have_button("Update #{@logan.name}")
+                click_button("Update #{@logan.name}")
                 expect(current_path).to eq(edit_cat_path(@logan.id))
             end
         end
