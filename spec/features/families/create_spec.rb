a7f341ea3c39640e77b37a3d0fbfebbf8b5bbb3e
Blake Sergesketter <blakesergesketter@gmail.com>
2025-02-24 06:37:25 -0600

    Start of testing for create action for joins table associations. SkeeYee.

---
+++ spec/features/families/create_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Create Families', type: :feature do
+    describe 'New Familiy Associations' do 
+        it 'has a form field to create new associations' do 
+            ...
+        end
+    end 
+end 
