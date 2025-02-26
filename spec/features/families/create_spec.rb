2db7f6d706ea74a095af12ca70c9ec175c89acef
Blake Sergesketter <blakesergesketter@gmail.com>
2025-02-26 06:10:12 -0600

    Cat and Dog objects.

--- spec/features/families/create_spec.rb
+++ spec/features/families/create_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Create Families', type: :feature do
-    describe 'New Familiy Associations' do 
+    describe 'New Familiy Associations' do
+        before :each do 
+            @karl = Cat.create!(name: 'Karl', age: 2, color: 'Brown')
+            @cowboy = Cat.create!(name: 'Cowboy', age: 2, color: 'Grey')
+            @bexley = Cat.create!(name: 'Bexley', age: 2, color: 'Brown')
+
+            @loki = Dog.create!(name: 'Loki', age: 5, color: 'Blue')
+            @nova = Dog.create!(name: 'Nova', age: 10, color: 'Yellow')
+            @heidi = Dog.create!(name: 'Heidi', age: 13, color: 'White')
+        end
+
         it 'has a form field to create new associations' do 
             visit new_families_path
 
