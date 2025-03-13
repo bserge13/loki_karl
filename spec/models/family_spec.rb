d1369e48c8fca820a20f0028e2700a9547a47d0f
Blake Sergesketter <blakesergesketter@gmail.com>
2025-03-13 06:39:53 -0500

    Model method testing passes. Big Yeet.

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Family, type: :model do
         it { should belong_to :cat }
         it { should belong_to :dog }
     end
+
+    describe 'model methods' do 
+        it '#name' do 
+            @karl = Cat.create!(name: 'Karl', age: 1, color: 'Brown')
+            @loki = Dog.create!(name: 'Loki', age: 5, color: 'Blue')
+            fam = Family.create!(dog_id: @loki.id, cat_id: @karl.id)
+
+            expect(fam.name).to eq("#{@loki.name} and #{@karl.name}")
+        end
+    end
 end
