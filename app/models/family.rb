14cf690527e74cf4419064870810e3df2e2fa5ec
Blake Sergesketter <blakesergesketter@gmail.com>
2025-03-13 06:29:28 -0500

    Implementation of model method for joins table.

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,4 +1,11 @@
 class Family < ApplicationRecord
     belongs_to :cat
     belongs_to :dog
+
+    def name
+        # require 'pry'; binding.pry
+        dog = Dog.find_by(id: self.dog_id)
+        cat = Cat.find_by(id: self.cat_id)
+        return "#{dog} and #{cat}"
+    end
 end
