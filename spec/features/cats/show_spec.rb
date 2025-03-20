7ccb97a010a0e05432509a67babefa87d896eca7
Blake Sergesketter <blakesergesketter@gmail.com>
2025-03-20 06:43:45 -0500

    Seudo coding. SkeeYee.

--- spec/features/cats/show_spec.rb
+++ spec/features/cats/show_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe 'Cats Features', type: :feature do
             
             click_link("#{@karl.name}'s Dogs")
             expect(current_path).to eq("/families/cats/#{@karl.id}")
+            # just doing it for the streaks
         end
     end
 end
