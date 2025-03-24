d93685c1a3a36c40083a5155db167c50ebc50d95
Blake Sergesketter <blakesergesketter@gmail.com>
2025-03-24 06:24:26 -0500

    Start of testing for delete action for joins table.

---
+++ spec/features/families/destroy_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Families Features' do
+    describe 'Families Destroy Feature' do 
+        before :each do 
+            @cowboy = Cat.create!(name: 'Cowboy', age: 1, color: 'Grey')
+            @bexley = Cat.create!(name: 'Bexley', age: 1, color: 'Brown')
+
+            @loki = Dog.create!(name: 'Loki', age: 5, color: 'Blue')
+            @heidi = Dog.create!(name: 'Heidi', age: 10, color: 'White')
+
+            @lc = Family.create!(dog_id: @loki.id, cat_id: @cowboy.id)
+            @hb = Family.create!(dog_id: @heidi.id, cat_id: @bexley.id)
+        end
+
+        it 'destroys an association' do
+            
+        end
+    end
+end
